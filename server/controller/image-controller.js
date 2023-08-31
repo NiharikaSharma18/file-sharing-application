9de3eacbba461b8fc9a17059e247c683d08d4301
Ritik Sachan <ritiksachan@Ritiks-MacBook-Air.local>
2023-09-01 00:10:16 +0530

    path correction

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -7,11 +7,9 @@ export const uploadImage = async (request, response) => {
   try {
     const file = await File.create(fileObj);
     console.log(file);
-    response
-      .status(200)
-      .json({
-        path: `https://file-sharing-application-backend.onrender.com//file/${file._id}`,
-      });
+    response.status(200).json({
+      path: `https://file-sharing-application-backend.onrender.com/file/${file._id}`,
+    });
   } catch (error) {
     console.error(error.message);
     response.status(500).json({ error: error.message });
